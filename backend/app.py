ef406477ca7d86f920befdd4b5472d15786c1c99
sakshi-mendhule <142528987+sakshi-mendhule@users.noreply.github.com>
2026-01-30 17:10:17 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,9 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "database-1.c5iwy40mkzj9.ap-northeast-1.rds.amazonaws.com",
+    "host": "database-1.cnsqeyms8oys.ap-northeast-1.rds.amazonaws.com",
     "user": "admin",
-    "password": "Cloud1234",
+    "password": "Root1234",
     "database": "cloud"
 }
 
@@ -116,6 +116,7 @@ def get_users():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     
+
 
 
 
